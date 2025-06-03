bc0e9810af95f0c7e35f5dcd6358c8e365eaf004
Roman Tyshchenko <udatos@gmail.com>
2025-06-03 17:31:15 +0300

    Add astro-pwa plugin, add @tailwindcss/typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,22 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [
+    solidJs(),
+    AstroPWA({
+      srcDir: "public",
+      filename: "sw.js",
+      registerType: "autoUpdate",
+      manifest: {},
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   adapter: cloudflare(),
 
   vite: {
