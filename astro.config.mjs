6b3e20a509a3c1b3718bdc00c8da302152bc9755
Roman Tyshchenko <udatos@gmail.com>
2025-06-08 11:17:42 +0300

    add sitename to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mytocalc.com',
   integrations: [
     solidJs(),
     AstroPWA({
